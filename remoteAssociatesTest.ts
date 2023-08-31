f5fbe917041cf8599c4e7a034bfbc88c333732b1
Tee.Wtn <teeranai.w@botanypetcare.co.th>
2023-08-31 17:24:33 +0700

    Update variable declarations to be more understanable and meaningful

--- remoteAssociatesTest.ts
+++ remoteAssociatesTest.ts
@@ -7,25 +7,22 @@ const phrases4: string[] = ['SORE LOSER', 'SORE THROAT', 'SORE SPOT'];
 const phrases5: string[] = ['DUCKBILL', 'BILLFOLD', 'DOLLAR BILL'];
 
 function getQuestionPart(phrases: string[]): string[] {
-
-  const trimPhrases: string[] = phrases.map(str => str.replace(/\s/g, ''));
-
-  const splitFirstPhrases: string[] = trimPhrases[0].split('');
-
-  const duplicateWord: string[] = [];
-  for(let i = 0; i < splitFirstPhrases.length; i++) {
-    let sumarray: string = splitFirstPhrases.slice(i, i + 2).join('');
-    
-    if (trimPhrases[1].includes(sumarray) === true) {
-      duplicateWord.push(sumarray);
+  const trimPhrases: string[] = phrases.map((str) => str.replace(/\s/g, ''));
+  const firstPhrase: string[] = trimPhrases[0].split('');
+  const finderPhrase: string[] = [];
+  for (let i = 0; i < firstPhrase.length; i++) {
+    let slicePhrase: string = firstPhrase.slice(i, i + 2).join('');
+    if (trimPhrases[1].includes(slicePhrase) === true) {
+      finderPhrase.push(slicePhrase);
     }
   }
+  const phrase: string[] = finderPhrase.filter((_, index) => index % 2 === 0);
+  const answerPhrase: string = phrase.join('');
+  const questionPart: string[] = trimPhrases.map((str) =>
+    str.replace(answerPhrase, '')
+  );
 
-  const oddIndexedElements: string[] = duplicateWord.filter((_, index) => index % 2 === 0);
-  const outputString: string = oddIndexedElements.join('');
-  const removedBillArray: string[] = trimPhrases.map(str => str.replace(outputString, ''));
-
-  return removedBillArray;
+  return questionPart;
 }
 
 console.log(`${phrases1} === ${getQuestionPart(phrases1)}`);
